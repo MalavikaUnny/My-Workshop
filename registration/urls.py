a19ffedbf2ad375cd8c6f401bbaae77b79a19662
MalavikaUnny <malavikaunny@gmail.com>
2016-06-07 14:01:39 +0530

    update

--- registration/urls.py
+++ registration/urls.py
@@ -3,10 +3,14 @@ from django.conf.urls import url
 from django.views.generic import TemplateView
 from registration.views import *
 
+
 urlpatterns = [
     url(r'^$', UserRegistrationView.as_view(), name='register_user'),
     url(r'^user/$', UserRegistrationView.as_view(), name='login'),
     url(r'^user/success/', TemplateView.as_view(template_name='register/user/success.html'),
         name='user_registration_success'),
-    url(r'^chocolate/add/', AddChocolateView.as_view(), name='add_chocolate')
+    url(r'^chocolate/add/', AddChocolateView.as_view(), name='add_chocolate'),
+    url(r'^chocolate/success/',TemplateView.as_view(template_name = 'register/chocolate/success.html'),name="success"),
+    url( r'^chocolate/info/(?P<choco_id>\d+)/$', ChocolateDetailsView.as_view(), name="chocolate_info"),
+    url(r'^user/profile/edit/$', UserProfileUpdateView.as_view(), name='update'),
 ]
--- registration/views.py
+++ registration/views.py
@@ -3,15 +3,22 @@ from django.views.generic import TemplateView
 from django.core.urlresolvers import reverse_lazy
 from braces.views import LoginRequiredMixin, AnonymousRequiredMixin
 from django.views.generic.edit import FormView, UpdateView
-from registration.forms import UserRegistrationForm
-from registration.models import User
+from django.views.generic import ListView
+from django.views.generic.detail import DetailView
+from registration.forms import UserRegistrationForm,user_fields,user_extra_fields
+from registration.models import User, Chocolate
 from registration.forms import ChocolateAddForm
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the registration index.")
-class Home(TemplateView):
+
+
+class Home(ListView):
     template_name="index.html"
+
+    def get_queryset(self):
+        return Chocolate.objects.all()
 
 class UserRegistrationView(AnonymousRequiredMixin, FormView):
     template_name = "register/user/register_user.html"
@@ -33,3 +40,33 @@ class AddChocolateView(FormView):
     def form_valid(self, form):
         form.save()
         return FormView.form_valid(self, form)
+
+class ChocolateDetailsView(DetailView):
+    template_name = "chocolate_detail.html"
+
+    def get_object(self, queryset=None):
+        choco_id = self.kwargs['choco_id']
+        obj = Chocolate.objects.get(id=choco_id)
+        if obj:
+            return obj
+        else:
+            raise Http404("No details Found.")
+
+class CurrentUserMixin(object):
+    model = User
+
+    def get_object(self, *args, **kwargs):
+      try:
+          obj = super(CurrentUserMixin, self).get_object(*args, **kwargs)
+      except AttributeError:
+          obj = self.request.user
+      return obj
+
+class UserProfileUpdateView(LoginRequiredMixin, CurrentUserMixin, UpdateView):
+    model = User
+    fields = user_fields + user_extra_fields
+    template_name_suffix = '_update_form'
+    success_url = '/register/user/profile/'
+
+
+
